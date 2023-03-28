de4e4509260f4bb88e3576ea652c380ead9e0112
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-03-28 17:22:37 +0500

    edit

--- index.js
+++ index.js
@@ -4,29 +4,26 @@ import Product from "./routes/Products.js"
 import Auth from "./routes/auth.js"
 import * as dotenv from "dotenv"
 dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+const api = express()
+api.use(express.json())
+api.use(express.urlencoded({ extended: true }));
 
 
 
 
-app.use(Auth)
-app.use(Product)
+api.use(Auth)
+api.use(Product)
 
 
-const startApp = () => {
+const startApi = () => {
     const PORT = process.env.PORT ?? 8000
     mongoose.set('strictQuery', true)
-    mongoose.connect(process.env.URI,
-        {
-            useNewUrlParser: true,
-        }
-    )
+    mongoose.connect(process.env.URI, { useNewUrlParser: true, })
         .then(() => console.log("MongoDB connected"))
-    app.listen(PORT, () => {
+        .catch((err) => console.log("MongoDB can't connect " + err))
+    api.listen(PORT, () => {
         console.log(`Server run on port ${PORT}`);
     })
 }
 
-startApp()
+startApi()
--- routes/Products.js
+++ routes/Products.js
@@ -7,14 +7,27 @@ const router = Router()
 router.get('/api/todos', async (req, res) => {
     const products = await Product.find().lean()
     req.headers['content-type'] = 'text/json'
-    res.status(200).send(products)
+    res.status(200).send(products.reverse())
+})
+
+router.get('/api/todo/detail/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    res.send(product)
 })
 
 router.post('/api/todo/add', async (req, res) => {
-    const { title, description, image, price } = req.body.product
     const token = getJWTTOKEN(req.headers.authorization)
     const product = await Product.create({ ...req.body.product, user: token.payload.userId })
     res.send(product)
+})
+
+router.post('/api/todo/edit/:id', async (req) => {
+    console.log(req.body.article);
+    // await Product.findByIdAndUpdate(req.params.id, req.body.article, { new: true })
+})
+
+router.delete('/api/todo/delete/:id', async (req, res) => {
+    await Product.findByIdAndRemove(req.params.id, { new: true })
 })
 
 
